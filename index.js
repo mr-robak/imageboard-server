d0ddfa7c8552ba14b48572597b0effdc64050273
Marcin Robak <mr.robak@gmail.com>
2020-05-28 14:50:24 +0200

    implement middleware authentication

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const jsonParser = express.json();
 
 const imageRouter = require("./routers/image");
 const userRouter = require("./routers/user");
-const loginRouter = require("./routers/auth");
+const authRouter = require("./routers/auth");
+
+const authMiddleware = require("./auth/middleware");
 
 const app = express();
 port = 4000;
@@ -11,8 +13,8 @@ port = 4000;
 app.use(jsonParser);
 
 app.use("/users", userRouter);
-app.use("/images", imageRouter);
-app.use("/login", loginRouter);
+app.use("/images", authMiddleware, imageRouter);
+app.use("/", authRouter);
 
 app.listen(port, () => {
   console.log(`Server is ON at :${port}`);
--- routers/user.js
+++ routers/user.js
@@ -1,5 +1,4 @@
 const { Router } = require("express");
-const bcrypt = require("bcrypt");
 const User = require("../models").user;
 
 const router = new Router();
@@ -13,26 +12,6 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.post("/signup", async (req, res, next) => {
-  try {
-    const { email, password, fullName } = req.body;
-    if (!email || !password || !fullName) {
-      res
-        .status(400)
-        .send("Please provide all parameters: email, password, fullName!");
-    } else {
-      // Here, when handing down the password to the create method we hash it.
-      const hashedPassword = bcrypt.hashSync(password, 10);
-      const newUser = await User.create({
-        email,
-        password: hashedPassword,
-        fullName,
-      });
-      res.json(newUser);
-    }
-  } catch (e) {
-    next(e);
-  }
-});
+
 
 module.exports = router;
